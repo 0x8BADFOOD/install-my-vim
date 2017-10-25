27a727a5d60d1b632ca44dd9e40c2a1f0a59d3af
Mobile Developer <app.mobile.developer@gmail.com>
2017-10-25 11:24:30 -0400

    Fixed installing curl

--- install.sh
+++ install.sh
@@ -3,12 +3,25 @@ echo "Installing vim"
 sudo apt-get install vim
 
 echo "Installing ctags"
-sudo apt-get install exuberant-ctags
+sudo apt-get install exuberant-ctags curl
+
+echo "Installing curl"
+sudo apt-get install curl
 
 echo "Installing spf13"
-curl https://j.mp/spf13-vim3 -L > spf13-vim.sh &&  spf13-vim.sh
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh
+sudo chmod +x spf13-vim.sh
+./spf13-vim.sh
 
 echo "Getting vim config"
 git clone https://github.com/0x8BADFOOD/dotfiles.git
 cp ./dotfiles/.vimrc.local $HOME/.vimrc.local
 rm -fr dotfiles
+rm -fr spf13-vim.sh
+
+
+#uninstalling:
+#sh .spf13-vim-3/uninstall.sh
+
+#mv ~/.vim/ ~/.spf13_vim
+#$ mv ~/.vimrc ~/.spf138vimrc
